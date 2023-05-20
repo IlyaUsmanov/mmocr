cdaaffcefd101cdbe2c9756f724ab45c9d2847e8
IlyaUsmanov <60606315+IlyaUsmanov@users.noreply.github.com>
2023-05-21 02:56:50 +0300

    Update cocotextv2.py

--- configs/textrecog/_base_/datasets/cocotextv2.py
+++ configs/textrecog/_base_/datasets/cocotextv2.py
@@ -1,13 +1,13 @@
 cocotextv2_textrecog_data_root = 'data/cocotextv2'
 
-cocotextv2_textdet_train = dict(
+cocotextv2_textrecog_train = dict(
     type='OCRDataset',
     data_root=cocotextv2_textrecog_data_root,
     ann_file='textrecog_train.json',
     filter_cfg=dict(filter_empty_gt=True, min_size=32),
     pipeline=None)
 
-cocotextv2_textdet_test = dict(
+cocotextv2_textrecog_test = dict(
     type='OCRDataset',
     data_root=cocotextv2_textrecog_data_root,
     ann_file='textrecog_val.json',
